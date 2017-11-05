cd8c5570560090307b74def878dca6e86d3bdc4e
陶江航 <taojianghang@xinzhentech.com>
2017-11-05 15:39:10 +0800

    initial commit

---
+++ src/main/java/com/ternence/designpattern/observer/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * create by 陶江航 at 2017/11/5 15:25
+ *
+ * @version 1.0
+ * @email ternence.tao@foxmail.com
+ * @description 观察者模式
+ */
+package com.ternence.designpattern.observer;
---
+++ src/main/java/com/ternence/designpattern/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * create by 陶江航 at 2017/11/5 15:24
+ *
+ * @version 1.0
+ * @email ternence.tao@foxmail.com
+ * @description 放置设计模式的代码的包
+ */
+package com.ternence.designpattern;
